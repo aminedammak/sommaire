a41360fbf24532d6fa4ec698855438b52c953fa6
Amine Dammak <amine.dammak@foodomarket.com>
2025-03-15 10:55:37 +0100

    Try sommaire button style

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
-import { Sparkles } from 'lucide-react';
+import { ArrowRight, Sparkles } from 'lucide-react';
+import Link from 'next/link';
 
 function HeroSection() {
   return (
@@ -24,7 +25,15 @@ function HeroSection() {
         <h2 className="text-lg sm:text-xl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600">
           Get a beautiful summary reel of the document in seconds.
         </h2>
-        <Button>Try Sommaire</Button>
+        <Button
+          variant={'link'}
+          className="text-white mt-6 text-base sm:text-lg lg:text-xl transition-all duration-300 rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lgmt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:bg-linear-to-l hover:no-underline font-bold"
+        >
+          <Link href={'/#pricing'} className="flex gap-2 items-center">
+            <span>Try Sommaire</span>
+            <ArrowRight className="animate-pulse" />
+          </Link>
+        </Button>
       </div>
     </section>
   );
