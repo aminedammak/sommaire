75a7b7df89de8508798d0a3437f6f573a3e26ef7
Amine Dammak <amine.dammak@foodomarket.com>
2025-03-17 10:42:31 +0100

    steps styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import DemoSection from '@/components/home/demo-section';
 import HeroSection from '@/components/home/hero-section';
-import { Button } from '@/components/ui/button';
+import HowItWorksSection from '@/components/home/how-it-works-section';
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection /> */}
-      {/* <HowItWorksSection /> */}
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+      </div>
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
